9ac1948e979533b6df698abdc333d77f6691e7c2
eobabs <eobabs@gmail.com>
2024-08-20 07:02:08 +0100

    third commit

---
+++ TaskFour.java
@@ -0,0 +1,11 @@
+public class TaskFour {
+    public static void main(String[] args) {
+
+        for (int count = 1; count <= 30; count+=5) {
+            
+		if (count % 4 == 0) System.out.print(count + " ");
+        }
+    }
+}
+
+// count++, ++count, count = count + 1, count +=1
